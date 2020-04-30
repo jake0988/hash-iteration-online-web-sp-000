e917c3b69a95485673a204a763d505728aeb6461
Jake G <jake098877@gmail.com>
2020-04-30 10:30:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if kids_name < 9
+    if birthday_kids[kids_name] < 9
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
