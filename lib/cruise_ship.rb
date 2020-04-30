cddab5d33808953f3bad10887c1599750adbaf2a
Jake G <jake098877@gmail.com>
2020-04-30 10:24:20 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,16 +7,14 @@
 # }
 
 def select_winner(passengers)
-  winner = " "
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with("A")
-      winner = name
-    end
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
 
 winner
-end
-
 # birthday_kids = {
 #   "Timmy" => 9,
 #   "Sarah" => 6,
